860532a6444ae8bc4ccb44d555d7213a83121a4d
Phil Chiu <whophilchiu@gmail.com>
2022-03-09 03:08:45 -0700

    Verbose cmake

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,17 +10,17 @@ cmake -B build -S "%SRC_DIR%\src" ^
 	-DCMAKE_MODULE_LINKER_FLAGS="/machine:x64 /FORCE:MULTIPLE"
 if errorlevel 1 exit 1
 
-cmake --build build -- -v -k 0
-cmake --build build -j 1 -- -v -k 0
-cmake --build build -j 1 -- -v -k 0
-cmake --build build -j 1 -- -v -k 0
-cmake --build build -j 1 -- -v -k 0
-cmake --build build -j 1 -- -v -k 0
-cmake --build build -j 1 -- -v -k 0
-cmake --build build -j 1 -- -v -k 0
-cmake --build build -j 1 -- -v -k 0
-cmake --build build -j 1 -- -v -k 0
-cmake --build build -j 1 -- -v -k 0
+cmake -v --build build -- -k 0
+cmake -v --build build -j 1 -- -k 0
+cmake -v --build build -j 1 -- -k 0
+cmake -v --build build -j 1 -- -k 0
+cmake -v --build build -j 1 -- -k 0
+cmake -v --build build -j 1 -- -k 0
+cmake -v --build build -j 1 -- -k 0
+cmake -v --build build -j 1 -- -k 0
+cmake -v --build build -j 1 -- -k 0
+cmake -v --build build -j 1 -- -k 0
+cmake -v --build build -j 1 -- -k 0
 if errorlevel 1 exit 1
 
 cmake --install build --prefix "%STDLIB_DIR%"
