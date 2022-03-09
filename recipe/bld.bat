81d31f98a921de5e6b0bcd4264e17120ee0b8789
Phil Chiu <whophilchiu@gmail.com>
2022-03-09 07:00:23 -0700

    /Zm20

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -10,7 +10,8 @@ cmake -B build -S "%SRC_DIR%\src" ^
 	-DCMAKE_MODULE_LINKER_FLAGS="/machine:x64 /FORCE:MULTIPLE"
 if errorlevel 1 exit 1
 
-cmake -build build -v -- -k 0
+set CL=/Zm20
+cmake --build build -v -- -k 0
 cmake --build build -v -j 1 -- -k 0
 if errorlevel 1 exit 1
 
