4e049bc7e2770a84c3564ad196ad2364dd7c7565
Koen Schmeets <hello@koenschmeets.nl>
2023-01-07 01:13:48 +0100

    Add CMAKE_ARGS to build scripts

--- recipe/build.sh
+++ recipe/build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 # FindOpenCascade.cmake bundled with OCP references the env var `CONDA_PREFIX`.
 # That is the right prefix when manually running CMake inside a conda env, but
 # the wrong one when using conda-build. Substitute the right prefix inline.
-CONDA_PREFIX="${PREFIX}" cmake -B build -S "${SRC_DIR}/src" \
+CONDA_PREFIX="${PREFIX}" cmake ${CMAKE_ARGS} -B build -S "${SRC_DIR}/src" \
 	-G Ninja \
 	-DCMAKE_BUILD_TYPE=Release
 
