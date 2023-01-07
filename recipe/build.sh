e718a9fd4caa54149300b62d4a2c4c89f6150703
Koen Schmeets <hello@koenschmeets.nl>
2023-01-07 16:02:12 +0100

    Find python

--- recipe/build.sh
+++ recipe/build.sh
@@ -6,6 +6,8 @@ set -euo pipefail
 # the wrong one when using conda-build. Substitute the right prefix inline.
 CONDA_PREFIX="${PREFIX}" cmake ${CMAKE_ARGS} -B build -S "${SRC_DIR}/src" \
 	-G Ninja \
+	-DPython3_FIND_STRATEGY="LOCATION" \
+	-DPython3_ROOT_DIR=$CONDA_PREFIX \
 	-DCMAKE_BUILD_TYPE=Release
 
 cmake --build build -j ${CPU_COUNT}
