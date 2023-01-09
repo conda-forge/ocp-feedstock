746169560c130a5149fb9c9fadec2e0c59047d04
Koen Schmeets <hello@koenschmeets.nl>
2023-01-09 02:39:19 +0100

    Use BUILD_PREFIX

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,12 +2,12 @@
 set -euo pipefail
 
 # find "${PREFIX}/lib"
-find "${BUILD_PREFIX}/lib"
+# find "${BUILD_PREFIX}/lib"
 
 # FindOpenCascade.cmake bundled with OCP references the env var `CONDA_PREFIX`.
 # That is the right prefix when manually running CMake inside a conda env, but
 # the wrong one when using conda-build. Substitute the right prefix inline.
-CONDA_PREFIX="${PREFIX}" cmake ${CMAKE_ARGS} -B build -S "${SRC_DIR}/src" \
+CONDA_PREFIX="${BUILD_PREFIX}" cmake ${CMAKE_ARGS} -B build -S "${SRC_DIR}/src" \
 	-G Ninja \
 	-DPython3_FIND_STRATEGY=LOCATION \
 	-DPython3_ROOT_DIR=${PREFIX} \
