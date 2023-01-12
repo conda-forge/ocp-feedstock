08e629835863c85290e52b38526735bfcbe46a71
Koen Schmeets <hello@koenschmeets.nl>
2023-01-12 13:13:53 +0100

    Test with adding BUILD_PREFIX

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,23 +1,12 @@
 #!/bin/sh
 set -euo pipefail
 
-# get vtk library for osx-arm64
-VTK_DIR=${PREFIX}
-if [[ "$OSTYPE" == "darwin"* && $OSX_ARCH == "arm64" ]]; then
-    VTK_DIR=${RECIPE_DIR}/vtk
-    conda list -p ${BUILD_PREFIX} > packages.txt
-    cat packages.txt
-    VTK_PACKAGE_VERSION=`grep vtk packages.txt | awk -F ' ' '{print $2}'`
-    export CONDA_SUBDIR=osx-arm64
-		conda create -y -p ${VTK_DIR} --no-deps vtk=${VTK_PACKAGE_VERSION}
-fi
-
 # FindOpenCascade.cmake bundled with OCP references the env var `CONDA_PREFIX`.
 # That is the right prefix when manually running CMake inside a conda env, but
 # the wrong one when using conda-build. Substitute the right prefix inline.
 CONDA_PREFIX="${PREFIX}" cmake ${CMAKE_ARGS} -B build -S "${SRC_DIR}/src" \
 	-G Ninja \
-	-DCMAKE_PREFIX_PATH="${VTK_DIR}" \
+	-DCMAKE_PREFIX_PATH="${BUILD_PREFIX}" \
 	-DPython3_FIND_STRATEGY=LOCATION \
 	-DPython3_ROOT_DIR=${PREFIX} \
 	-DPython3_EXECUTABLE=${PREFIX}/bin/python \
