a32de6b27abfc10d07ca26ae5479532b3779053c
Phil Chiu <whophilchiu@gmail.com>
2023-01-15 13:12:37 -0700

    Fix order of commands

--- recipe/bld.bat
+++ recipe/bld.bat
@@ -1,3 +1,6 @@
+copy /Y "%RECIPE_DIR%\CMakeLists.txt" "%SRC_DIR%\src\CMakeLists.txt"
+if errorlevel 1 exit 1
+
 cmake -B build -S "%SRC_DIR%\src" ^
 	-G Ninja ^
 	-DCMAKE_BUILD_TYPE=Release ^
@@ -7,7 +10,6 @@ cmake -B build -S "%SRC_DIR%\src" ^
 	-DCMAKE_MODULE_LINKER_FLAGS="/machine:x64 /FORCE:MULTIPLE"
 if errorlevel 1 exit 1
 
-copy /Y "%RECIPE_DIR%\CMakeLists.txt" "%SRC_DIR%\src\CMakeLists.txt"
 cmake --build build -v -- -k 0
 cmake --build build -v -j 1 -- -k 0
 if errorlevel 1 exit 1
--- recipe/build.sh
+++ recipe/build.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 set -euo pipefail
 
+cp ${RECIPE_DIR}/CMakeLists.txt ${SRC_DIR}/src/CMakeLists.txt
+
 cmake -B build -S "${SRC_DIR}/src" \
 	-G Ninja \
 	-DPython3_ROOT_DIR=${PREFIX} \
 	-DCMAKE_BUILD_TYPE=Release
-
-cp ${RECIPE_DIR}/CMakeLists.txt ${SRC_DIR}/src/CMakeLists.txt
 
 cmake --build build -j ${CPU_COUNT}
 cmake --install build --prefix "${STDLIB_DIR}"
