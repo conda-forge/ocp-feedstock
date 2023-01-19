ddea33d3af2a6d02fa879fc936e2513761bdc1d0
AU <adam-urbanczyk@users.noreply.github.com>
2023-01-19 08:53:03 +0100

    Update builds.sh with the new vtk workaround

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,16 +1,9 @@
 #!/bin/sh
 set -euo pipefail
 
-# get vtk library for osx-arm64
-VTK_DIR=${PREFIX}
-if [[ "$OSTYPE" == "darwin"* && $OSX_ARCH == "arm64" ]]; then
-    VTK_DIR=/tmp/vtk
-    conda list -p ${BUILD_PREFIX} > packages.txt
-    cat packages.txt
-    VTK_PACKAGE_VERSION=`grep vtk packages.txt | awk -F ' ' '{print $2}'`
-    export CONDA_SUBDIR=osx-arm64
-		conda create -y -p ${VTK_DIR} --no-deps vtk=${VTK_PACKAGE_VERSION}
-		VTK_DIR="${VTK_DIR}/lib/cmake/vtk-${VTK_PACKAGE_VERSION}"
+# only use the dummy cmake file for osx-arm64
+if ! [[ "$OSTYPE" == "darwin"* && $OSX_ARCH == "arm64" ]]; then
+    rm FindVTK.cmake
 fi
 
 # FindOpenCascade.cmake bundled with OCP references the env var `CONDA_PREFIX`.
@@ -18,10 +11,10 @@ fi
 # the wrong one when using conda-build. Substitute the right prefix inline.
 CONDA_PREFIX="${PREFIX}" cmake ${CMAKE_ARGS} -B build -S "${SRC_DIR}/src" \
 	-G Ninja \
-	-DVTK_DIR="${VTK_DIR}" \
 	-DPython3_FIND_STRATEGY=LOCATION \
 	-DPython3_ROOT_DIR=${PREFIX} \
 	-DPython3_EXECUTABLE=${PREFIX}/bin/python \
+	-DCMAKE_MODULE_PATH=. \
 	-DCMAKE_BUILD_TYPE=Release
 
 cmake --build build -j ${CPU_COUNT}
